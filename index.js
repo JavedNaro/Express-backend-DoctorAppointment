5bf7bff966952f7ecd4fe5a87cd5eb01baf58652
Javed Naro <muhammadjavednaro089@gmail.com>
2023-12-01 23:26:16 -0800

    commit 3

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-import express from "express"
-import cookieParser from "cookie-parser"
-import cors from "cors"
-import mongoose from "mongoose"
-import dotenv from 'dotenv'
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+const corsOptions = {
+  origin: true,
+};
+
+app.get("/", (req, res) => {
+  res.send("Api is working");
+});
+
+//midleware 
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOption));
+
+app.listen(port, ()=>{
+    console.log("Server is running on port" + post);
+});
